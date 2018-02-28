0c2722b41262e491272504a244c76084391959f8
bailongctui <bailongctui@gmail.com>
2018-02-28 18:02:20 +0800

    init
    初始化react 项目框架  +todolist
    redux+react-router+react-router-dom

--- src/reducers/todoreducer.js
+++ src/reducers/todoreducer.js
@@ -1,5 +1,6 @@
 export default function (state = [], action) {
     switch (action.type) {
+        
         case "ADD_TODO_LIST":
             var newState = [...state];
             newState.push(action.payload);
